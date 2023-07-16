b98d61673118558266b8ba85b9ea3273176e5ee8
ahmdsquare <78866805+ahmdsquare@users.noreply.github.com>
2023-07-16 21:11:26 +0300

    U

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('website.welcome');
+    return view('website.index');
 });
