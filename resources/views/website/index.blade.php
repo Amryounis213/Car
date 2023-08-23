f3666244fe3a95c7dfc3b1eb5d86b7ea8bee4594
ahmdsquare <78866805+ahmdsquare@users.noreply.github.com>
2023-08-23 21:35:51 +0300

    U

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -55,7 +55,7 @@
                     <div class="main__title main__title--first">
                         <h2>Featured cars</h2>
 
-                        <a href="cars.html" class="main__link">View more<svg xmlns="http://www.w3.org/2000/svg"
+                        <a href="{{ route('search') }}" class="main__link">View more<svg xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24">
                                 <path
                                     d="M17.92,11.62a1,1,0,0,0-.21-.33l-5-5a1,1,0,0,0-1.42,1.42L14.59,11H7a1,1,0,0,0,0,2h7.59l-3.3,3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l5-5a1,1,0,0,0,.21-.33A1,1,0,0,0,17.92,11.62Z" />
